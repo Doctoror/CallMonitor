bbf499bb465620e66e65d9b4e63ece1993341fc9
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-11 16:16:38 +0200

    Leave doc on use case

--- domain/src/main/java/com/dd/callmonitor/domain/phonenumbers/NormalizePhoneNumberUseCase.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/phonenumbers/NormalizePhoneNumberUseCase.kt
@@ -5,6 +5,12 @@ import java.util.Locale
 
 class NormalizePhoneNumberUseCase(private val locale: Locale) {
 
+    /**
+     * Normalize phone number to E164 format.
+     *
+     * If not possible, will only normalize by removing the spaces and extra characters like
+     * whitespaces and dashes.
+     */
     operator fun invoke(number: String?): String {
         val normalized = PhoneNumberUtils.normalizeNumber(number)
         return if (normalized.isBlank()) {
