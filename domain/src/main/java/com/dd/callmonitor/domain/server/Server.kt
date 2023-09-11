378dcd64a94cee156010d030716a6d68af1c59f1
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-11 16:16:47 +0200

    Add doc for Server

--- domain/src/main/java/com/dd/callmonitor/domain/server/Server.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/server/Server.kt
@@ -4,7 +4,13 @@ import java.net.InetAddress
 
 interface Server {
 
+    /**
+     * Starts the server and binds to the specified host
+     */
     suspend fun start(host: InetAddress)
 
+    /**
+     * Stops the server if running. If not running will do nothing.
+     */
     suspend fun stopIfRunning()
 }
