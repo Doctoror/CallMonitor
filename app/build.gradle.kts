9741565b1b68c7417bc8fe44d1d79687d3f2f65b
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-11 23:25:33 +0200

    Remove LGPL from excludes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}" //TODO what's this?
             excludes += "/META-INF/INDEX.LIST"
             excludes += "/META-INF/io.netty.*"
         }
