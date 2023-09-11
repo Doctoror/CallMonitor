71a3af08098f2e71ce7677b5d1684c59ac55549b
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-11 16:12:07 +0200

    Leave doc on use case

--- domain/src/main/java/com/dd/callmonitor/domain/calllog/GetCallLogUseCase.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/calllog/GetCallLogUseCase.kt
@@ -2,6 +2,10 @@ package com.dd.callmonitor.domain.calllog
 
 import com.dd.callmonitor.domain.util.Either
 
+/**
+ * Note for reviewers: the use cases are needed to decouple "what to do" from "how to do". This
+ * makes the business logic decoupled from repositories.
+ */
 class GetCallLogUseCase(private val callLogRepository: CallLogRepository) {
 
     suspend operator fun invoke(): Either<CallLogError, List<CallLogEntry>> =
