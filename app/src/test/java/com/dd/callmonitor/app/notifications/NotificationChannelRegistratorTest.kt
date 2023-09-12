a1682bfa9b6a366c6fe7676c3d11ba90071f9b17
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-12 12:44:31 +0200

    Add NotificationChannelRegistratorTest

---
+++ app/src/test/java/com/dd/callmonitor/app/notifications/NotificationChannelRegistratorTest.kt
@@ -0,0 +1,48 @@
+package com.dd.callmonitor.app.notifications
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import com.dd.callmonitor.R
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+class NotificationChannelRegistratorTest {
+
+    private val channelId = "channelId"
+    private val notificationManager: NotificationManager = mockk()
+    private val resources = RuntimeEnvironment.getApplication().resources
+
+    private val underTest = NotificationChannelRegistrator(notificationManager, resources)
+
+    @Test
+    @Config(sdk = [Build.VERSION_CODES.N_MR1])
+    fun doesNotThrowNoSuchMethodErrorPreOreo() {
+        underTest(channelId)
+    }
+
+    @Test
+    @Config(sdk = [Build.VERSION_CODES.O])
+    fun registerPostOreo() {
+        every { notificationManager.createNotificationChannel(any()) } returns Unit
+
+        underTest(channelId)
+
+        verify {
+            notificationManager.createNotificationChannel(
+                NotificationChannel(
+                    channelId,
+                    resources.getString(R.string.notification_channel_name_server_status),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+            )
+        }
+    }
+}
