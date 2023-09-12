debecfed8169ad8b6b9ed3d9749c069999abdfe1
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-12 17:16:42 +0200

    Implement equals, hashCode and toString in Either

--- domain/src/main/java/com/dd/callmonitor/domain/util/Either.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/util/Either.kt
@@ -5,6 +5,9 @@ package com.dd.callmonitor.domain.util
  *
  * Can be considered as a simplified version of
  * https://github.com/arrow-kt/arrow-core/blob/master/arrow-core-data/src/main/kotlin/arrow/core/Either.kt
+ *
+ * Cannot be a `data` class because we cannot support `copy` functionality which will break the
+ * contract by allowing to set both values to null or non-null.
  */
 class Either<L, R> private constructor(
     private val left: L?,
@@ -19,6 +22,23 @@ class Either<L, R> private constructor(
     } else {
         onRight(right!!)
     }
+
+    override fun equals(other: Any?) = this === other || (
+            other is Either<*, *> &&
+                    other.left == left &&
+                    other.right == right
+            )
+
+    /**
+     * Auto-generated
+     */
+    override fun hashCode(): Int {
+        var result = left?.hashCode() ?: 0
+        result = 31 * result + (right?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString() = "Either(left = $left, right = $right)"
 
     companion object {
 
