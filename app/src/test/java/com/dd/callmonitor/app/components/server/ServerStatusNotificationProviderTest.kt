32709479760d0e16829d14cac515caf0cb1265df
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-12 13:22:14 +0200

    Add ServerStatusNotificationProviderTest

---
+++ app/src/test/java/com/dd/callmonitor/app/components/server/ServerStatusNotificationProviderTest.kt
@@ -0,0 +1,61 @@
+package com.dd.callmonitor.app.components.server
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.dd.callmonitor.R
+import com.dd.callmonitor.app.components.main.MainActivity
+import com.dd.callmonitor.app.di.KoinStoppingTest
+import com.dd.callmonitor.app.notifications.NOTIFICATION_CHANNEL_ID_SERVER_STATUS
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import kotlin.test.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+class ServerStatusNotificationProviderTest : KoinStoppingTest {
+
+    private val context = RuntimeEnvironment.getApplication()
+    private val underTest = ServerStatusNotificationProvider()
+
+    @Test
+    fun provides() {
+        val contentTitle = "Some title"
+
+        val output = underTest.provide(context, contentTitle)
+
+        assertEquals(
+            NotificationCompat
+                .Builder(
+                    context,
+                    NOTIFICATION_CHANNEL_ID_SERVER_STATUS
+                )
+                .setAutoCancel(true)
+                .setContentIntent(mainActivityContentIntent())
+                .setContentTitle(contentTitle)
+                .setSmallIcon(R.drawable.baseline_power_settings_new_24)
+                .build()
+                .toString(),
+            output.toString()
+        )
+    }
+
+    @Test
+    fun contentIntentLeadsToMainActivity() {
+        val output = underTest.provide(context, "Some title")
+
+        assertEquals(
+            mainActivityContentIntent(),
+            output.contentIntent
+        )
+    }
+
+    private fun mainActivityContentIntent() = PendingIntent.getActivity(
+        context,
+        0,
+        Intent(context, MainActivity::class.java),
+        PendingIntent.FLAG_IMMUTABLE
+    )
+}
