7f08794a048522e99680c9dbce586ed49fcb1c5b
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-13 22:03:30 +0200

    Add comment

--- presentation/src/main/java/com/dd/callmonitor/presentation/calllog/CallLogPresenter.kt
+++ presentation/src/main/java/com/dd/callmonitor/presentation/calllog/CallLogPresenter.kt
@@ -9,6 +9,9 @@ import kotlinx.coroutines.launch
 class CallLogPresenter(
     private val callLogViewModelUpdater: CallLogViewModelUpdater,
     private val observeCallLogUseCase: ObserveCallLogUseCase,
+    // Arguably, having the property below public is a Law of Demeter violation, however, this
+    // accessor will be used only once and having a viewModel here provides more benefits of
+    // ensuring the same instance for a presenter
     val viewModel: CallLogViewModel
 ) : ViewModel() {
 
