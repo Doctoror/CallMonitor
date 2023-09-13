e34e7e0d52b40a44644b45e10b1d52a5fa18df0d
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-13 10:51:28 +0200

    Use callbackFlow for ObserveWifiConnectivityUseCase

--- domain/src/main/java/com/dd/callmonitor/domain/connectivity/ObserveWifiConnectivityUseCase.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/connectivity/ObserveWifiConnectivityUseCase.kt
@@ -3,55 +3,17 @@ package com.dd.callmonitor.domain.connectivity
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkRequest
-import androidx.annotation.AnyThread
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.onStart
+import java.net.InetAddress
 
 class ObserveWifiConnectivityUseCase(
     private val connectivityManager: ConnectivityManager,
     private val isActiveNetworkWifiUseCase: IsActiveNetworkWifiUseCase
 ) {
-
-    private val lock = Any()
-
-    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
-
-        override fun onAvailable(network: Network) {
-            // Note for reviewers:
-            // here we just terminate with exception if link properties cannot be derived.
-            //
-            // Additional use case can be created not to throw and handle the case where the ip
-            // address cannot be derived and let user figure out their address manually.
-            //
-            // However, to reduce the scope of the project I will just leave out this feature and
-            // terminate early.
-            val linkProperties = requireNotNull(connectivityManager.getLinkProperties(network)) {
-                "No LinkProperties for network $network"
-            }
-
-            val siteLocalAddress = linkProperties
-                .linkAddresses
-                .map { it.address }
-                .firstOrNull { it.isSiteLocalAddress }
-            // Note for reviewers: same as comment for linkProperties/requireNotNull.
-                ?: throw IllegalStateException("Site-local address not found")
-
-
-            connectivityStateFlow.value = ConnectivityState.Connected(siteLocalAddress)
-        }
-
-        override fun onLost(network: Network) {
-            connectivityStateFlow.value = ConnectivityState.Disconnected
-        }
-    }
-
-    private val connectivityStateFlow =
-        MutableStateFlow<ConnectivityState>(ConnectivityState.Unknown)
-
-    private var networkCallbackRegistered = false
 
     /**
      * @return a [Flow] that emits
@@ -59,28 +21,30 @@ class ObserveWifiConnectivityUseCase(
      * - [ConnectivityState.Connected] if connected to a Wi-Fi network
      * - [ConnectivityState.Disconnected] if not connected to a Wi-Fi network
      */
-    @AnyThread
-    operator fun invoke(): Flow<ConnectivityState> =
-        combine(
-            connectivityStateFlow.onStart(::connectivityStateFlowOnStart),
-            // Note for the reviewers
-            // This part is controversial, because we unnecessarily synchronize on lock and check
-            // the callback flag on every emission of networkState. However, I can't think of a
-            // better way to reliably encapsulate registering and unregistering a network callback
-            // without making specific public methods for those, which would in turn make using this
-            // use case not as seamless as it is now. As of now, no users of this use case have to
-            // think about managing the connectivity manager as it would automatically unregister
-            // once the last subscriber of connectivityStateFlow unsubscribes.
-            connectivityStateFlow.subscriptionCount
-        ) { state, count ->
-            if (count > 0) {
-                registerCallbacksIfNotRegistered()
-            } else {
-                unregisterCallbacksIfRegistered()
+    operator fun invoke(): Flow<ConnectivityState> = callbackFlow {
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+
+            override fun onAvailable(network: Network) {
+                trySend(ConnectivityState.Connected(network.getSiteLocalAddressOrThrow()))
             }
 
-            state
+            override fun onLost(network: Network) {
+                trySend(ConnectivityState.Disconnected)
+            }
         }
+
+        connectivityManager.registerNetworkCallback(
+            NetworkRequest
+                .Builder()
+                .addTransportType(android.net.NetworkCapabilities.TRANSPORT_WIFI)
+                .build(),
+            networkCallback
+        )
+
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
+    }.onStart(::connectivityStateFlowOnStart)
 
     /**
      * [ConnectivityManager.NetworkCallback] does not emit default value when requested network is
@@ -106,30 +70,24 @@ class ObserveWifiConnectivityUseCase(
         }
     }
 
-    @AnyThread
-    private fun registerCallbacksIfNotRegistered() {
-        synchronized(lock) {
-            if (!networkCallbackRegistered) {
-                networkCallbackRegistered = true
-
-                connectivityManager.registerNetworkCallback(
-                    NetworkRequest
-                        .Builder()
-                        .addTransportType(android.net.NetworkCapabilities.TRANSPORT_WIFI)
-                        .build(),
-                    networkCallback
-                )
-            }
+    private fun Network.getSiteLocalAddressOrThrow(): InetAddress {
+        // Note for reviewers:
+        // here we just terminate with exception if link properties cannot be derived.
+        //
+        // Additional use case can be created not to throw and handle the case where the ip
+        // address cannot be derived and let user figure out their address manually.
+        //
+        // However, to reduce the scope of the project I will just leave out this feature and
+        // terminate early.
+        val linkProperties = requireNotNull(connectivityManager.getLinkProperties(this)) {
+            "No LinkProperties for network $this"
         }
-    }
 
-    @AnyThread
-    private fun unregisterCallbacksIfRegistered() {
-        synchronized(lock) {
-            if (networkCallbackRegistered) {
-                networkCallbackRegistered = false
-                connectivityManager.unregisterNetworkCallback(networkCallback)
-            }
-        }
+        return linkProperties
+            .linkAddresses
+            .map { it.address }
+            .firstOrNull { it.isSiteLocalAddress }
+        // Note for reviewers: same as comment for linkProperties/requireNotNull.
+            ?: throw IllegalStateException("Site-local address not found")
     }
 }
