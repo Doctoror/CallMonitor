6a8d4ca64241bf990b3fe5e0d0c1d2a7ee5fd6f8
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-14 23:32:19 +0200

    Add doc

--- domain/src/main/java/com/dd/callmonitor/domain/calllog/CallLogRepository.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/calllog/CallLogRepository.kt
@@ -3,6 +3,11 @@ package com.dd.callmonitor.domain.calllog
 import com.dd.callmonitor.domain.util.Either
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Because we don't want the `domain` layer to depend on `data` layer, we need this interface to
+ * a repository in the data layer. This would be a part of "Interface Adapters" layer in Uncle Bob's
+ * architecture.
+ */
 interface CallLogRepository {
 
     fun observeCallLog(): Flow<Either<CallLogError, List<CallLogEntry>>>
