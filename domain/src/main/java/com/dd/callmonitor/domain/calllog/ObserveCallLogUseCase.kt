8f3669c0907361ebc838c2f9b9f222e419b00ea9
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-14 23:34:52 +0200

    Update doc

--- domain/src/main/java/com/dd/callmonitor/domain/calllog/ObserveCallLogUseCase.kt
+++ domain/src/main/java/com/dd/callmonitor/domain/calllog/ObserveCallLogUseCase.kt
@@ -6,6 +6,11 @@ import kotlinx.coroutines.flow.Flow
 /**
  * Note for reviewers: the use cases are needed to decouple "what to do" from "how to do". This
  * makes the business logic decoupled from repositories.
+ *
+ * For example, refactoring a repository interface would not leak out to a `presentation` layer, and
+ * a single change in a repository would need to be changed in a single use case, but if the
+ * repository would have been used many times in other layers a change in the repository would
+ * render lots of changes across the app.
  */
 class ObserveCallLogUseCase(private val callLogRepository: CallLogRepository) {
 
