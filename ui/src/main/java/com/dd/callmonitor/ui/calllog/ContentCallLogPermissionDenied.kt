286cf78c3bc131b64597a094f019d8718ee25f45
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-09-15 12:34:37 +0200

    Adjust for small content

--- ui/src/main/java/com/dd/callmonitor/ui/calllog/ContentCallLogPermissionDenied.kt
+++ ui/src/main/java/com/dd/callmonitor/ui/calllog/ContentCallLogPermissionDenied.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ErrorOutline
 import androidx.compose.material3.Button
@@ -21,10 +23,13 @@ import com.dd.callmonitor.ui.R
 
 @Composable
 fun ContentCallLogPermissionDenied(launchPermissionRequest: () -> Unit) {
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
+            .verticalScroll(scrollState),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
