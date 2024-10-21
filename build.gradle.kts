0bfd3c585c7f5e1b6e9d234edf677cb7eaf67862
Yaroslav Mytkalyk <docd1990@gmail.com>
2024-10-21 13:37:19 +0200

    Update Kotlin and Compose
    Accompanist: 0.36.0
    Compose BOM: 2024.10.00
    Compose Compiler: 1.5.15
    Kotlin: 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version libs.versions.agp apply false
-    id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
     kotlin("plugin.serialization") version libs.versions.kotlin apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
